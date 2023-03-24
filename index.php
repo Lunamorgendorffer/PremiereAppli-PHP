422b5c66abc548d0de35e060b64bc479a61501be
Lunamorgendorffer <nabdou68@gmail.com>
2023-03-24 16:58:11 +0100

    style page

--- index.php
+++ index.php
@@ -4,48 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Ajout produit</title>
 </head>
 <body>
-    <h1>ACCUEIL</h1>
-    <nav>
-        <ul>
-            <li><a href="index.php">ACCUEIL</a></li>
-            <li><a href="recap.php">RECAPUTILATIF</a></li>
-        </ul>
-    </nav>
-    
-   
-
-    <h2>Ajouter un produit</h2>
-
-    <form action="traitement.php?action=ajouter" method="post"> 
-    <!--  La méthode employée ici est POST, pour ne pas "polluer" l'URL avec les données du
-    formulaire. -->
-        <p>
-            <label>
-                Nom du produit: 
-                <input type="text" name="name">
-            </label>
-        </p>
-        <p>
-            <label>
-                Prix du produit: 
-                <input type="number" step="any" name="price" >
-            </label>
-        </p>
-        <p>
-            <label>
-                Quantité désirée: 
-                <input type="number" name="qtt" value="1">
-            </label>
-        </p>
-        <p>
-        <input type="submit" name="submit" value="Ajouter le produit">
-        </p>
-    </form>
-
-    
+    <ul>
+        <li><a  href="#home">Home</a></li>
+        <li><a href='index.php'>AJOUT PRODUIT</a></li>
+        <li><a href="recap.php">RECAPUTILATIF</a></li>
+    </ul>
+    <section class="inputs">
+        <h2>Ajouter un produit</h2>
+        <div class="cards">
+            <div class="card">
+                <form action="traitement.php?action=ajouter" method="post"> 
+        <!--  La méthode employée ici est POST, pour ne pas "polluer" l'URL avec les données du
+        formulaire. -->
+                <p>
+                <label class="info">
+                    Nom du produit:     
+                    <input type="text" name="name">
+                </label>
+                </p>
+                <p>
+                <label class="info">
+                    Prix du produit: 
+                    <input type="number" step="any" name="price" >
+                </label>
+                </p>
+                <p>
+                <label class="info">
+                    Quantité désirée: 
+                    <input type="number" name="qtt" value="1">
+                </label>
+                </p>
+                <p>
+                <input type="submit" name="submit" value="Ajouter le produit">
+                </p>
+                </form>
+            </div>
+        </div>
+    </section>
     
 </body>
 </html>
--- recap.php
+++ recap.php
@@ -7,16 +7,16 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <title>Récapitulatif des produits</title>
 </head>
 <body>
-    <h1>RECAPITULATIF</h1>
-    <nav>
-        <ul>
-            <li><a href="http://localhost/appli/index.php">ACCUEIL</a></li>
-            <li><a href="http://localhost/appli/recap.php">RECAPUTILATIF</a></li>
-        </ul>
-    </nav>
+<ul>
+        <li><a class="active" href="#home">Home</a></li>
+        <li><a href='index.php'>AJOUT PRODUIT</a></li>
+        <li><a href="recap.php">RECAPUTILATIF</a></li>
+    </ul>
     <?php 
     //Pour afficher le nombre de produits présents en session à tout moment,
     if(isset($_SESSION['products'])){
@@ -33,7 +33,7 @@ session_start();
         
     }
     else{ // sinon afficher sous forme de  tableau les informations demandées 
-        echo "<table>",
+        echo "<table class='styled-table'>",
                    " <thead>",
                         "<tr>",
                             "<th>#</th>",
@@ -41,6 +41,8 @@ session_start();
                             "<th>Prix</th>",
                             "<th>Quantité</th>",
                             "<th>Total</th>",
+                            "<th>Montant Total</th>",
+                            "<th>Option</th>",
                         "</tr>",
                     "</thead>",
                 "<tbody>";
@@ -58,7 +60,7 @@ session_start();
                             </td>",
                         "<td>".number_format($product['total'],2,",","&nbsp;")."&nbsp;€</td>", // Afficher le prix TOTAL du ou des  produit en cours, avec un espace insécable et le symbole € à la fin
                         // ajout d'un bouton supprimer ligne
-                        "<td id='button'><a href='traitement.php?action=supprimer&id=".$index."' ><button>Supprimer</button><a></td>",
+                        "<td id='button'><a href='traitement.php?action=supprimer&id=".$index."' ><button class='supp'>Supprimer</button><a></td>",
                     "</tr>";
              $totalGeneral += $product['total'] ; // Ajouter le total de chaque produit au total général
         }
